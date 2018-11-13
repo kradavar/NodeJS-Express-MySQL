85c9617a442f481fb062a49eb64c471529a5f125
Darya Krasava <kradavar@gmail.com>
2018-11-13 10:09:24 +0300

    add module for creating events

---
+++ addEvent.js
@@ -0,0 +1,36 @@
+const mysql = require("mysql");
+const config = require("./config");
+const connection = mysql.createConnection(config);
+
+const addEvent = (name, start, end, userID) => {
+  connection.connect(err => {
+    if (err) {
+      return console.warn("error: " + err.message);
+    }
+    const createEventsList = `create table if not exists eventsList(
+                          id int primary key auto_increment,
+                          name varchar(255)not null,
+                          start varchar(255)not null,
+                          end varchar(255)not null,
+                          user_id int not null 
+                      )`;
+
+    connection.query(createEventsList, (err, results, fields) => {
+      if (err) {
+        console.warn("error: " + err.message);
+      }
+    });
+
+    const insert = `INSERT INTO eventsList(name,start,end,user_id)
+           VALUES(?,?,?,?)`;
+    const event = [name, start, end, userID];
+    connection.query(
+      insert,
+      event,
+      (err, results, fields) => err && console.error(err.message)
+    );
+    connection.end(err => err && console.log(err.message));
+  });
+};
+
+module.exports = addEvent;
--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+const addEvent = require("./addEvent");
 
-var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "NodeTest"
-});
-
-app.get("/", function(req, res) {
-  connection.query("select * from test_table", function(
-    error,
-    results,
-    fields
-  ) {
-    if (error) throw error;
-    res.send(results[0]);
-  });
-});
-
-app.listen(3001, function() {
-  console.log("Example app listening on port 3001!");
-});
+addEvent("Test 3", "2018", "2018", 2);
